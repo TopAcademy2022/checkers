87d0b45a6738152853a55f774c354d20e0cdd47c
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-09-19 16:02:07 +0300

    Added figure placement. Closed #6

---
+++ Checkers/GridGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Checkers
+{
+	public class GridGame : Grid
+	{
+		private const byte _countCell = 8;
+
+		public GridGame(Window parentElement)
+		{
+			parentElement.Content = this;
+
+			for (byte i = 0; i < _countCell; i++)
+			{
+				this.RowDefinitions.Add(new RowDefinition());
+				this.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+		}
+	}
+}
