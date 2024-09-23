9dbde0687c6f3b7e9c5c3fd0f70488574466f762
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2024-09-23 20:48:17 +0300

    Added logical game field. Closed #8 (#14)

---
+++ Checkers/Graphics/GraphicalGameField.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Checkers.Logic;
+
+namespace Checkers.Graphics
+{
+    public class GraphicalGameField : Grid
+    {
+        private GameField _gameField;
+
+        public GraphicalGameField(Window parentElement)
+        {
+            this._gameField = new GameField();
+
+			parentElement.Content = this;
+
+            for (byte i = 0; i < GameField.COUNT_ROWS; i++)
+            {
+                RowDefinitions.Add(new RowDefinition());
+                ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+    }
+}
---
+++ Checkers/Graphics/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using Checkers.Graphics;
+
+namespace Checkers
+{
+    public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			GraphicalGameField game = new GraphicalGameField(this);
+		}
+	}
+}
