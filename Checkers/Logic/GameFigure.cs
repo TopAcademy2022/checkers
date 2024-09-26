9d9ff528e4c52aaddbfa699bfad243c93afdecb2
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2024-09-26 21:37:27 +0300

    Added logical game figure. Closed #17 (#27)

---
+++ Checkers/Logic/GameFigure.cs
@@ -0,0 +1,40 @@
+﻿namespace Checkers.Logic
+{
+	public class GameFigure
+	{
+		private bool _isQueen;
+
+		public enum GameColor : byte
+		{
+			White = 0,
+			Black = 1
+		}
+
+		private GameColor _color;
+
+		public enum DirectionMovement
+		{
+			Up = 0,
+			Down = 1
+		}
+
+		private DirectionMovement _directionMovement;
+
+		public GameFigure(GameColor color, DirectionMovement directionMovement)
+		{
+			this._isQueen = false;
+			this._color = color;
+			this._directionMovement = directionMovement;
+		}
+
+		public GameColor GetColor()
+		{
+			return this._color;
+		}
+
+		public void SetQueen()
+		{
+			this._isQueen = true;
+		}
+	}
+}
