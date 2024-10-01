625deae544164e6139f2807691149e6d4c86a91b
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-10-01 21:38:32 +0300

    Add possible figure movement. Closed #20

--- Checkers/Logic/GameField.cs
+++ Checkers/Logic/GameField.cs
@@ -7,6 +7,9 @@ namespace Checkers.Logic
 	{
 		private readonly ILogger<GameField> _logger;
 
+
+		private readonly List<KeyValuePair<char, int>> _occupiedСell;
+
 		private readonly List<KeyValuePair<char, int>> _emptyCells;
 
 		private readonly Dictionary<GameFigure, KeyValuePair<char, int>> _checkersField;
@@ -21,11 +24,11 @@ namespace Checkers.Logic
 		{
 			try
 			{
-				if (this._emptyCells.Contains(destinationCell))
+				if (this._occupiedСell.Contains(destinationCell))
 				{
-					this._emptyCells.Add(this._checkersField[gameFigure]);
+					this._occupiedСell.Add(this._checkersField[gameFigure]);
 					this._checkersField[gameFigure] = destinationCell;
-					this._emptyCells.Remove(destinationCell);
+					this._occupiedСell.Remove(destinationCell);
 				}
 				else
 				{
@@ -49,7 +52,7 @@ namespace Checkers.Logic
 			{
 				if (rowNumber >= START_ROW_NUMBER && rowNumber <= FINISH_ROW_NUMBER)
 				{
-					result = this._emptyCells.Where(cell => cell.Value == rowNumber).ToList();
+					result = this._occupiedСell.Where(cell => cell.Value == rowNumber).ToList();
 				}
 				else
 				{
@@ -129,8 +132,8 @@ namespace Checkers.Logic
 			List<KeyValuePair<char, int>> result = new List<KeyValuePair<char, int>>(); ///< All possible positions
 			KeyValuePair<char, int> presentPosition = this._checkersField[gameFigure]; ///< Current position
 
-			const uint CHAR_CODE_A = 97; ///< Letter index
-			const uint CHAR_CODE_H = 104; ///< Letter index
+			const uint CHAR_CODE_A = 97; ///< Char code from Ascii table
+			const uint CHAR_CODE_H = 104; ///< Char code from Ascii table
 
 			DirectionMovement movement = gameFigure.GetDirectionMovement(); ///< Direction Movement
 
@@ -184,15 +187,23 @@ namespace Checkers.Logic
 			using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
 			this._logger = factory.CreateLogger<GameField>();
 
+			this._occupiedСell = new List<KeyValuePair<char, int>>();
+
 			this._emptyCells = new List<KeyValuePair<char, int>>();
+
 			this._checkersField = new Dictionary<GameFigure, KeyValuePair<char, int>>();
 
 			char[] chars1 = { 'a', 'c', 'e', 'g' };
 
 			foreach (char symbol in chars1)
 			{
+				this._occupiedСell.Add(new KeyValuePair<char, int>(symbol, 1));
+				this._occupiedСell.Add(new KeyValuePair<char, int>(symbol, 3));
+				this._occupiedСell.Add(new KeyValuePair<char, int>(symbol, 7));
+
 				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 1));
 				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 3));
+				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 5));
 				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 7));
 			}
 
@@ -200,7 +211,12 @@ namespace Checkers.Logic
 
 			foreach (char symbol in chars2)
 			{
+				this._occupiedСell.Add(new KeyValuePair<char, int>(symbol, 2));
+				this._occupiedСell.Add(new KeyValuePair<char, int>(symbol, 6));
+				this._occupiedСell.Add(new KeyValuePair<char, int>(symbol, 8));
+
 				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 2));
+				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 4));
 				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 6));
 				this._emptyCells.Add(new KeyValuePair<char, int>(symbol, 8));
 			}
@@ -222,13 +238,94 @@ namespace Checkers.Logic
 					this._checkersField.Add(gameFigure, emptyCell);
 				}
 			}
+
+			foreach (KeyValuePair<char, int> a in this._occupiedСell)
+			{
+				this._emptyCells.Remove(a);
+			}
 		}
 
+		/*!
+		* @brief Possible figure movement.
+		 * @param[in] gameFigure - to determine the course.
+		 * @return List of every field coordinate.
+		 */
 		public List<KeyValuePair<char, int>> GetPossibleFigureMovement(GameFigure gameFigure)
 		{
 			List<KeyValuePair<char, int>> result = new List<KeyValuePair<char, int>>();
 
+			List<KeyValuePair<char, int>> leftDiagonal = this.GetLeftDiagonal(gameFigure);
+			List<KeyValuePair<char, int>> ridhtDiagonal = this.GetRightDiagonal(gameFigure);
 
+			switch (gameFigure.GetIsQueen()) ///< Queen check
+			{
+				case false:
+
+					if (this._emptyCells.Contains(leftDiagonal.FirstOrDefault())) ///< Checking an empty cell
+					{
+						result.Add(leftDiagonal.FirstOrDefault()); ///< Add the result to our List
+					}
+					else
+					{
+						if (this._checkersField.Where(el => el.Value.Key == leftDiagonal.FirstOrDefault().Key && el.Value.Value == leftDiagonal.FirstOrDefault().Value).First().Key.GetColor() != gameFigure.GetColor())
+						{
+							for (int i = 1; i < leftDiagonal.Count; i += 2)
+							{
+								if (this._emptyCells.Contains(leftDiagonal[i])) ///< Checking an empty cell
+								{
+									result.Add(leftDiagonal[i]); ///< Add the result to our List
+								}
+								else
+								{
+									break;
+								}
+							}
+						}
+					}
+
+					if (this._emptyCells.Contains(ridhtDiagonal.FirstOrDefault())) ///< Checking an empty cell
+					{
+						result.Add(ridhtDiagonal.FirstOrDefault()); ///< Add the result to our List
+					}
+					else
+					{
+						if (this._checkersField.Where(el => el.Value.Key == leftDiagonal.FirstOrDefault().Key && el.Value.Value == leftDiagonal.FirstOrDefault().Value).First().Key.GetColor() != gameFigure.GetColor())
+						{
+							for (int i = 1; i < ridhtDiagonal.Count; i += 2)
+							{
+								if (this._emptyCells.Contains(ridhtDiagonal[i])) ///< Checking an empty cell
+								{
+									result.Add(ridhtDiagonal[i]); ///< Add the result to our List
+								}
+								else
+								{
+									break;
+								}
+							}
+						}
+					}
+
+					break;
+
+				case true:
+
+					foreach (KeyValuePair<char, int> left in leftDiagonal)
+					{
+						if (this._emptyCells.Contains(left)) ///< Checking an empty cell
+						{
+							result.Add(left); ///< Add the result to our List
+						}
+					}
+					foreach (KeyValuePair<char, int> ridht in ridhtDiagonal)
+					{
+						if (this._emptyCells.Contains(ridht)) ///< Checking an empty cell
+						{
+							result.Add(ridht); ///< Add the result to our List
+						}
+					}
+
+					break;
+			}
 
 			return result;
 		}
