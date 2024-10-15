b98d002e8968cc0f146d4589c3860172d22e8c3d
global133 <sergej.chuev.98@mail.ru>
2024-10-15 20:21:48 +0300

    Added story movement to the database. Closed #30

--- Checkers/Logic/DatabaseConnection.cs
+++ Checkers/Logic/DatabaseConnection.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+
 namespace Checkers.Logic
 {
     public class DatabaseConnection : DbContext
     {
         public DatabaseConnection() => this.Database.EnsureCreated();
 
+        public DbSet<CheckerMove> HistoryMoves => Set<CheckerMove>();
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=checkers.db");
+        }
+
+        public void AddCheckerMove(CheckerMove checkerMove)
+        {
+            this.HistoryMoves.Add(checkerMove);
+            this.SaveChanges();
         }
     }
 }
